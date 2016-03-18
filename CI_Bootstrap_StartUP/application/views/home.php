733a948e852b16aa03d905a94dd75ac8caf06d5e
Kiril Kirkov <kirilkirkov@users.noreply.github.com>
2016-03-18 15:41:21 +0200

    Update home.php

--- CI_Bootstrap_StartUP/application/views/home.php
+++ CI_Bootstrap_StartUP/application/views/home.php
@@ -1,11 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-
-<div id="center-bg">
-    <div class="container">
-        <h1 class="home-other">Тест</h1>
-        
-    </div>
+<div class="container">
+        <h1>Тест</h1>
 </div>
-

c46bcbe44281fd738005336f5d00c42ce7de1058
Kiril Kirkov <kirilkirkov@users.noreply.github.com>
2016-03-18 15:37:59 +0200

    db

---
+++ CI_Bootstrap_StartUP/db.sql
@@ -0,0 +1,27 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE IF NOT EXISTS `articles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `basic_info` varchar(500) NOT NULL,
+  `header` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `big_image` varchar(255) NOT NULL,
+  `info` longtext NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `home_show` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin.admin');

f708fec5846e96b33a1f608101821d68b05312b6
Kiril Kirkov <kirilkirkov@users.noreply.github.com>
2016-03-23 16:15:59 +0200

    Update db.sql

--- CI_Bootstrap_StartUP/db.sql
+++ CI_Bootstrap_StartUP/db.sql
@@ -3,20 +3,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `articles` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `basic_info` varchar(500) NOT NULL,
-  `header` varchar(255) NOT NULL,
+  `basic_description` varchar(500) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `thumb` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `big_image` varchar(255) NOT NULL,
-  `info` longtext NOT NULL,
+  `description` longtext NOT NULL,
   `url` varchar(255) NOT NULL,
-  `home_show` tinyint(1) NOT NULL DEFAULT '0',
+  `category` varchar(20) NOT NULL,
+  `visibility` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 CREATE TABLE IF NOT EXISTS `history` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `activity` varchar(255) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
   `time` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -28,7 +35,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin.admin');

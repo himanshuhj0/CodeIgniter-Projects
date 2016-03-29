c55cdddd2fd3b6aa53ff5333562055e4f77d3162
Kiril Kirkov <kirilkirkov@users.noreply.github.com>
2016-03-29 16:28:05 +0300

    Update View_article.php

--- CI_Bootstrap_StartUP/application/controllers/View_article.php
+++ CI_Bootstrap_StartUP/application/controllers/View_article.php
@@ -7,20 +7,24 @@ class View_article extends MY_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->model('Articles_model');
+        $this->load->helper('text');
     }
 
     public function index($id) {
         $data = array();
         $head = array();
-        $head['title'] = 'Title information‎';
+        $data_header['header_info'] = $this->Articles_model->getArticles('header_info');
+        $data_header['slider_info'] = $this->Articles_model->getArticles('slider');
+        $this->load->vars($data_header);
+        $data['article'] = $this->Articles_model->getOneArticle($id);
+        if($data['article'] === null) {
+        	show_404();
+        }
+        $head['title'] = $data['article']['title'];
         $description = url_title(character_limiter(strip_tags($data['article']['description']), 130));
         $description = str_replace("-", " ", $description).'..';
         $head['description'] = $description;
-        $head['keywords'] = 'key,words,for,seo';
-        $data['article'] = $this->Articles_model->getOneArticle($id);
-        if($data['article'] === null) {
-            show_404();
-        }
+        $head['keywords'] = 'подобри, себе си, здравословна храна, планински походи, power aerobic, тийм билдинг, последния оцелял';
         $this->render('view_article', $head, $data);
     }
 

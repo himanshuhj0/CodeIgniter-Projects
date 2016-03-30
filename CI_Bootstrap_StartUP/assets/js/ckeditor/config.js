7b160480f1271d9d67195c31f699c8cb1cad3342
Kiril Kirkov <kiro_tyson@abv.bg>
2016-03-30 09:45:36 +0300

    added ckeditor html source plugin for highlighting

--- CI_Bootstrap_StartUP/assets/js/ckeditor/config.js
+++ CI_Bootstrap_StartUP/assets/js/ckeditor/config.js
@@ -7,5 +7,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.allowedContent = true;
+        config.allowedContent = true;
+        config.extraPlugins = 'codemirror';
 };

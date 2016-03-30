001938c7fcd2d6f31fa8de2491271f266ff07da1
Kiril Kirkov <kirilkirkov@users.noreply.github.com>
2016-03-30 12:57:14 +0300

    Update Home.php
    commented pagination

--- CI_Bootstrap_StartUP/application/controllers/Home.php
+++ CI_Bootstrap_StartUP/application/controllers/Home.php
@@ -3,10 +3,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends MY_Controller {
+    
+    private $num_rows = 20;
 
     public function __construct() {
         parent::__construct();
         $this->load->model('Articles_model');
+        $this->load->helper(array('pagination'));
     }
 
     public function index() {
@@ -16,6 +19,11 @@ class Home extends MY_Controller {
         $head['description'] = 'Description info';
         $head['keywords'] = 'key,words,for,seo';
         $data['articles'] = $this->Articles_model->getArticles('test1');
+        /* Pagination
+        $rowscount = $this->News_model->news_count();
+		$data['news'] = $this->News_model->get_all($this->num_rows, $page);
+		$data['links_pagination'] = pagination('новини', $rowscount, $this->num_rows);
+        */
         $this->render('home', $head, $data);
     }
 
